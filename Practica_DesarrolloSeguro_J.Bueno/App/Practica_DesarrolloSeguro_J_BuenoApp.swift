e3da10c3ddc510e67b06c84755ed06a0da9fbfcf
Boncroix <boncroix@gmail.com>
2024-05-15 19:42:40 +0200

    Initial Commit

---
+++ Practica_DesarrolloSeguro_J.Bueno/App/Practica_DesarrolloSeguro_J_BuenoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Practica_DesarrolloSeguro_J_BuenoApp.swift
+//  Practica_DesarrolloSeguro_J.Bueno
+//
+//  Created by Jose Bueno Cruz on 15/5/24.
+//
+
+import SwiftUI
+
+@main
+struct Practica_DesarrolloSeguro_J_BuenoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Practica_DesarrolloSeguro_J.Bueno/Presentation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Practica_DesarrolloSeguro_J.Bueno
+//
+//  Created by Jose Bueno Cruz on 15/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
